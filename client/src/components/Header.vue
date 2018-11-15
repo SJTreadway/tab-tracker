e6c035345f71830baed23c7829a242b0779e93b5
Steven Treadway <steven.treadway@fractalindustries.com>
2018-11-15 13:01:27 -0500

    adds vuex and sets up store.

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -14,15 +14,21 @@
 
     <v-spacer></v-spacer>
 
-    <v-toolbar-items>
+    <v-toolbar-items v-if="!$store.state.isUserLoggedIn">
       <v-btn flat dark @click="navigateTo({ name: 'login' })">
         Login
       </v-btn>
     </v-toolbar-items>
 
-    <v-toolbar-items>
+    <v-toolbar-items v-if="!$store.state.isUserLoggedIn">
       <v-btn flat dark @click="navigateTo({ name: 'register' })">
         Sign Up
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-toolbar-items v-if="$store.state.isUserLoggedIn">
+      <v-btn flat dark @click="navigateTo({ name: 'logout' })">
+        Logout
       </v-btn>
     </v-toolbar-items>
   </v-toolbar>
